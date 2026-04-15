c3762102d1c296edf413da2e8088c9d217feb5ca
rkn720 <113858969+rkn720@users.noreply.github.com>
2026-04-15 23:15:20 +0530

    Add files via upload

---
+++ python_basics_ravikumar.py.py
@@ -0,0 +1,14 @@
+name = "Ravikumar"
+age = 27
+height = 5.9
+is_student = True
+print(name, age, height, is_student)
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_squared = age ** 2
+print("Age in months", age_in_months)
+print("Age in days", age_in_days)
+print("Remainder when age is divided by 7", remainder)
+print("Age raised to the power of 2", age_squared)
